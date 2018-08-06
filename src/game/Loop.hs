273060fc4e4f75e1822f44920e3e9389e3b9978c
Cmdv <vincent@cmdv.me>
2018-08-06 10:20:18 +0100

    took functions out of loop and put them into moves

--- src/game/Loop.hs
+++ src/game/Loop.hs
@@ -2,7 +2,7 @@ module Game.Loop where
 
 import Control.Monad.Trans.State
 import Data.Traversable
-import Game.Moves (robotDirToPos)
+import Game.Moves (robotDirToPos, updateRobotPos, updateHouses)
 import Game.Types
 
 gameLoop :: StateT [GameState] IO ()
@@ -32,37 +32,14 @@ singleGameLoop ogs = do
             then
               [GameState
                 { gameStateTurnNumber = newTurnNumber
-                , gameStateRobots = updateRobot newPos curRobot allRobots 0
+                , gameStateRobots = updateRobotPos newPos curRobot allRobots 0
                 , gameStateMoves = tail $ gameStateMoves lastOldGameState
                 , gameStateVisitedHouses = updateH 0
                 }]
             else
               [GameState
                 { gameStateTurnNumber = newTurnNumber
-                , gameStateRobots = updateRobot newPos curRobot allRobots 1
+                , gameStateRobots = updateRobotPos newPos curRobot allRobots 1
                 , gameStateMoves = tail $ gameStateMoves lastOldGameState
                 , gameStateVisitedHouses = updateH 1
                 }]
-
-updateRobot :: Position -> Robot -> [Robot] -> Int -> [Robot]
-updateRobot p curRobot allRobots num =
-  (\r -> if robotName r == robotName curRobot
-         then Robot (robotName curRobot) p (robotDelivered curRobot + num)
-         else r
-  ) <$> allRobots
-
--- create/update a house to houses
-updateHouses :: Position -> [House] -> Int -> [House]
-updateHouses pos allHouses num | null allHouses = [House pos num]
-                               | pos == (0,0) = allHouses
-                               | not $ isHouseThere allHouses pos = allHouses <> [House pos num]
-                               | isHouseThere allHouses pos =
-                                   (\r -> if housePosition r == pos
-                                          then House pos (houseNumberOfDeliveries r + num)
-                                          else r
-                                   ) <$> allHouses
-                               | otherwise = allHouses
-
--- check if a house Position alread exists
-isHouseThere :: [House] -> Position -> Bool
-isHouseThere hs pos =  or $ (\r -> housePosition r == pos) <$> hs
--- src/game/Moves.hs
+++ src/game/Moves.hs
@@ -25,3 +25,27 @@ createRobots :: [String] -> [Robot]
 createRobots = fmap createRobot
   where
     createRobot name' = Robot name' (0,0) 0
+
+
+updateRobotPos :: Position -> Robot -> [Robot] -> Int -> [Robot]
+updateRobotPos p curRobot allRobots num =
+  (\r -> if robotName r == robotName curRobot
+         then Robot (robotName curRobot) p (robotDelivered curRobot + num)
+         else r
+  ) <$> allRobots
+
+-- create/update a house to houses
+updateHouses :: Position -> [House] -> Int -> [House]
+updateHouses pos allHouses num | null allHouses = [House pos num]
+                               | pos == (0,0) = allHouses
+                               | not $ isHouseThere allHouses pos = allHouses <> [House pos num]
+                               | isHouseThere allHouses pos =
+                                   (\r -> if housePosition r == pos
+                                          then House pos (houseNumberOfDeliveries r + num)
+                                          else r
+                                   ) <$> allHouses
+                               | otherwise = allHouses
+
+-- check if a house Position alread exists
+isHouseThere :: [House] -> Position -> Bool
+isHouseThere hs pos =  or $ (\r -> housePosition r == pos) <$> hs
