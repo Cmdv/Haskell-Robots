d0bac00e81ae9cff53c7b7372154889693104e00
Cmdv <vincent@cmdv.me>
2018-08-09 22:02:18 +0100

    edit the commands to have less IO functions

--- src/game/Commands.hs
+++ src/game/Commands.hs
@@ -6,75 +6,69 @@ import           Game.Loop
 import qualified Game.Texts as GT
 import           Game.Types
 
--- check which command was issued
+
+runGameState :: [GameState] -> IO [GameState]
+runGameState gss = do
+  (_, newState) <- runStateT gameLoop gss
+  return newState
+
+-- Run the command that was given via command line
 whichCommand :: String -> GameState -> IO ()
 whichCommand str gs =
    case str of
-     "run:all" -> commandRunAll gs
-     "run:one" -> commandRunOne gs
-     "robots" -> commandTotalRobots gs
+
+     -- run the whole simulation
+     "run:all" -> do
+       print =<< runGameState [gs]
+       putStrLn GT.successSim
+
+     -- run the first move
+     "run:one" -> do
+       print =<< runGameState [defaultSingleMove gs]
+       putStrLn GT.successSim
+
+     -- show the status of robots after simulation
+     "robots" -> do
+       print . robotLocation =<< runGameState [gs]
+       putStrLn GT.successRobot
+         where
+           robotLocation s = gameStateRobots (last s)
+
+     -- show the help options
      "help" -> putStrLn GT.showHelp
-     "viewTurn" -> commandViewSpecificTurn gs
-     "presents" -> commandPresents gs
-     "houses" -> commandHouses gs
+
+     -- select any turn number to view a snap shot of state at given turn
+     "viewTurn" -> do
+        stateValue <- runGameState [gs]
+        putStrLn $ "Chose a number between 1-" ++ show (lastGameTurn stateValue)
+        i <- getLine
+        let rewound = drop (read i) stateValue
+        case rewound of
+          [] -> print "You asked for a state that is before the beginning of time!"
+          (x : _) -> print x
+        where
+          lastGameTurn s = gameStateTurnNumber (last s)
+
+     -- total presents
+     "presents" -> do
+        stateValue <- runGameState [gs]
+        putStrLn $ "\n🎁 " ++ show (robotLocation stateValue) ++ " presents where delivered!!"
+        where
+          robotLocation s = do
+            let houses = gameStateVisitedHouses (last s)
+            foldr (\a b -> houseNumberOfDeliveries a + b ) 0 houses
+
+     -- Show houses with 'x' or more deliveries
+     "houses" -> do
+        stateValue <- runGameState [gs]
+        putStrLn "Chose a number show houses with 'x' or more presents delivered:"
+        i <- getLine
+        let results = filterHouses (read i) (allHouses stateValue)
+        case results of
+          [] -> print "There are no houses to show sorry!!"
+          r -> print r
+          where
+            allHouses s = gameStateVisitedHouses (last s)
+            filterHouses i = filter (\h -> houseNumberOfDeliveries h >= i)
+
      _ -> putStrLn "unknown command sorry!"
-
--- run the whole simulation
-commandRunAll :: GameState -> IO ()
-commandRunAll gs = do
-  (_, newState) <- runStateT gameLoop [gs]
-  print newState
-  putStrLn GT.successSim
-
--- run one move
-commandRunOne :: GameState -> IO ()
-commandRunOne gs = do
-  (_, newState) <- runStateT gameLoop [defaultSingleMove gs]
-  print newState
-  putStrLn GT.successSim
-
--- select any turn number to view a snap shot of state at given turn
-commandViewSpecificTurn :: GameState -> IO ()
-commandViewSpecificTurn gs = do
-  (_, newState) <- runStateT gameLoop [gs]
-  putStrLn $ "Chose a number between 1-" ++ show (lastGameTurn newState)
-  i <- getLine
-  let rewound = drop (read i) newState
-  case rewound of
-    [] -> print "You asked for a state that is before the beginning of time!"
-    (x : _) -> print x
-  where
-    lastGameTurn s = gameStateTurnNumber (last s)
-
--- run the whole simulation
-commandTotalRobots :: GameState -> IO ()
-commandTotalRobots gs = do
-  (_, newState) <- runStateT gameLoop [gs]
-  print $ robotLocation newState
-  putStrLn GT.successRobot
-  where
-    robotLocation s = gameStateRobots (last s)
-
--- Total presents
-commandPresents :: GameState -> IO ()
-commandPresents gs = do
-  (_, newState) <- runStateT gameLoop [gs]
-  putStrLn $ "\n🎁 " ++ show (robotLocation newState) ++ " presents where delivered!!"
-  where
-    robotLocation s = do
-      let houses = gameStateVisitedHouses (last s)
-      foldr (\a b -> houseNumberOfDeliveries a + b ) 0 houses
-
--- Show houses with 'x' or more deliveries
-commandHouses :: GameState -> IO ()
-commandHouses gs = do
-  (_, newState) <- runStateT gameLoop [gs]
-  putStrLn "Chose a number show houses with 'x' or more presents delivered:"
-  i <- getLine
-  let results = filterHouses (read i) (allHouses newState)
-  case results of
-    [] -> print "There are no houses to show sorry!!"
-    r -> print r
-  where
-    allHouses s = gameStateVisitedHouses (last s)
-    filterHouses i = filter (\h -> houseNumberOfDeliveries h >= i)
